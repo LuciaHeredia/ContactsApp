a9eb3767101e35ba0eecb7f06b9bc1a32938a787
Lucia Heredia <LuciaHeredia22@gmail.com>
2024-02-13 01:49:33 +0200

    other fragments

---
+++ app/src/main/java/com/example/contactsapp/ui/fragments/ContactsFragment.java
@@ -0,0 +1,4 @@
+package com.example.contactsapp.ui.fragments;
+
+public class ContactsFragment {
+}
---
+++ app/src/main/java/com/example/contactsapp/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.contactsapp.utils;
+
+public class Constants {
+
+    //* API *//
+    public static final String BASE_URL = "https://api.genderize.io/";
+
+}
