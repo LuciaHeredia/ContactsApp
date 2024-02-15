9c111343bafc7e13608fd99ea9e400ea734df2f5
Lucia Heredia <LuciaHeredia22@gmail.com>
2024-02-15 05:49:26 +0200

    changes

--- app/src/main/java/com/example/contactsapp/ui/fragments/AddContactFragment.java
+++ app/src/main/java/com/example/contactsapp/ui/fragments/AddContactFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,6 +12,11 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.contactsapp.R;
 import com.example.contactsapp.databinding.FragmentAddContactBinding;
+import com.example.contactsapp.utils.Constants;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddContactFragment extends Fragment {
     private FragmentAddContactBinding binding;
@@ -31,8 +37,21 @@ public class AddContactFragment extends Fragment {
     }
 
     private void addContact() {
-        // TODO
-        goToContacts();
+        String fName = binding.etFirstName.getText().toString();
+        String lName = binding.etLastName.getText().toString();
+        String gender; // TODO: from API
+        String phone = binding.etPhone.getText().toString();
+        String email = binding.etEmail.getText().toString();
+        String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+
+        if(fName.isEmpty() || lName.isEmpty() || phone.isEmpty() || email.isEmpty()) {
+            Toast.makeText(getActivity(), Constants.MSG_FIELDS_MANDATORY,Toast.LENGTH_SHORT).show();
+
+        } else {
+
+
+            goToContacts();
+        }
     }
 
     private void goToContacts() {
